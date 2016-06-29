cecde66d143771dd8b84aee4d857f3249b6d82f6
Hank Donnay <hdonnay@gmail.com>
2016-06-29 09:28:19 -0500

    Jira: logging, commenting, initial window
    This adds better debug logging with the 'D' flag.
    
    Button 3 expansion has been improved.
    
    Comments can be posted.
    
    An initial window can be supplied.

--- main.go
+++ main.go
@@ -13,21 +13,26 @@ import (
 )
 
 var (
-	authStr = flag.String("a", "", "`username:password` combination")
+	authStr     = flag.String("a", "", "`username:password` combination")
+	debugEnable = flag.Bool("D", false, "enable debug output")
+
+	debug func(string, ...interface{}) = func(_ string, _ ...interface{}) {}
 )
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "\t%s [options] server\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\t%s [options] server [win]\n\n", os.Args[0])
 	flag.PrintDefaults()
 	fmt.Fprintf(os.Stderr, "\n")
 	fmt.Fprintf(os.Stderr, "Credentials are looked for in a OS-specific secret store (linux only currently),\n")
 	fmt.Fprintf(os.Stderr, "then in ~/.jira-creds. The 'a' flag will override both. They're all expected to\n")
 	fmt.Fprintf(os.Stderr, "be in the same format.\n\n")
+	fmt.Fprintf(os.Stderr, "If a window name is supplied, it will be opened instead of \"my-issues\".\n\n")
 }
 
 func init() {
 	flag.Usage = usage
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func main() {
@@ -41,6 +46,14 @@ func main() {
 	signal.Notify(sig, os.Interrupt, os.Kill)
 	flag.Parse()
 	jURL := flag.Arg(0)
+	if jURL == "" {
+		log.Fatal("need to specify jira server")
+	}
+	if *debugEnable {
+		debug = func(f string, v ...interface{}) {
+			log.Output(2, fmt.Sprintf(f, v...))
+		}
+	}
 
 	// Ideally we'd use some OAuth2 stuff, but it requires server-side setup for some reason.
 	auth.User, auth.Pass, auth.Err = secretsOS(jURL)
@@ -86,6 +99,7 @@ type comments struct {
 
 func (cs *comments) format(w io.Writer) {
 	for _, c := range cs.Comments {
+		debug("visibility: %#v\n", c.Visibility)
 		fmt.Fprintf(w, "\nComment by %s (%s)\n", c.Author.Name, c.Updated)
 		fmt.Fprintf(w, "\n\t%s\n", wrap(c.Body, "\t"))
 	}
--- ui.go
+++ ui.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -18,6 +19,7 @@ import (
 type win struct {
 	*acme.Win
 	Title string
+	Issue bool
 
 	reload func(*win)
 	put    func(*win)
@@ -40,53 +42,89 @@ func (w *win) Put() {
 	}
 }
 
-const addrdelim = "/[ \t\\n<>()\\[\\]]/"
+const addrdelim = "/[ \t\\n<>()\\[\\]\"']/"
 
-func (w *win) loadText(e *acme.Event) {
-	//	if len(e.Text) == 0 && e.Q0 < e.Q1 {
-	var err error
-	w.Addr("#%d,#%d+%s", e.Q0, e.Q0, addrdelim)
-	e.Q0, e.Q1, err = w.ReadAddr()
+// This is modeled on a similar set of functions that seem to be in every acme program.
+func (w *win) expand(e *acme.Event) {
+	w.Addr("#%d-%s", e.Q0, addrdelim)
+	l, _, err := w.ReadAddr()
 	if err != nil {
 		log.Println(err)
 	}
-	e.Q1--
-	w.Addr("#%d,#%d", e.Q0, e.Q1)
 
+	w.Addr("#%d+%s", e.Q0, addrdelim)
+	r, _, err := w.ReadAddr()
+	if err != nil {
+		log.Println(err)
+	}
+
+	if r < l {
+		l = 0
+	} else {
+		l++
+	}
+
+	w.Addr("#%d,#%d", l, r)
 	data, err := w.ReadAll("xdata")
 	if err != nil {
 		log.Println(err)
 	}
+	e.Q0 = l
+	e.Q1 = r
 	e.Text = data
 }
 
 func (w *win) loop(ui *UI) {
 	defer ui.exit(w.Title)
 	for e := range w.EventChan() {
-		//log.Printf("event: %q %q\n", e.C2, string(e.Text))
 		switch e.C2 {
 		case 'x', 'X': // button 2
-			switch cmd := strings.TrimSpace(string(e.Text)); cmd {
-			case "Get":
-				w.Reload()
+			debug("event: %q %q\n", e.C2, string(e.Text))
+			cmd := strings.TrimSpace(string(e.Text))
+			switch cmd {
 			case "Put":
 				w.Put()
-			case "Del":
-				w.Ctl("del")
+				fallthrough
+			case "Get":
+				w.Reload()
+				continue
 			case "New":
 				ui.createIssue()
+				continue
 			case "Search":
-				// more complicated
-				log.Println("asked to search, going to slink away...")
-			default:
-				w.WriteEvent(e)
+				// launch a search window
+				ui.err("Asked to Search, but I'm too dumb D:\n")
+				continue
+			case "Advance":
+				if !w.Issue {
+					ui.err("Can't Advance something other than a ticket.\n")
+					continue
+				}
+				ui.err("Asked to Advance, but I'm too dumb D:\n")
+				continue
+			}
+			if strings.HasPrefix(cmd, "Search") {
+				query := strings.TrimSpace(cmd[6:])
+				// do a search and return results
+				_ = query
 			}
 		case 'l', 'L': // button 3
-			w.loadText(e)
-			if !ui.look(string(e.Text)) {
-				w.WriteEvent(e)
+			debug("event: %x %q %q\n", e.Flag, e.C2, string(e.Text))
+			if ui.look(string(e.Text)) {
+				// we found it, or made it!
+				continue
+			}
+			// this could be a built-in, punt without expanding
+			if e.Flag&0x1 != 0 {
+				break
+			}
+			w.expand(e)
+			debug("expanded: %d-%d %q\n", e.Q0, e.Q1, string(e.Text))
+			if ui.look(string(e.Text)) {
+				continue
 			}
 		}
+		w.WriteEvent(e)
 	}
 }
 
@@ -143,7 +181,12 @@ func (u *UI) start(prefix string, j *jira.Client) {
 	u.j = j
 	u.projRe = regexp.MustCompile("^(" + strings.Join(r, "|") + ")-[0-9]+")
 	u.Unlock()
-	u.look("my-issues")
+
+	if flag.NArg() < 2 {
+		u.look("my-issues")
+		return
+	}
+	u.look(strings.Join(flag.Args()[1:], " "))
 }
 
 func (u *UI) new(title string) *win {
@@ -156,6 +199,7 @@ func (u *UI) new(title string) *win {
 		u.err(err.Error())
 		return nil
 	}
+	debug("spawning: %q\n", title)
 	w.Title = title
 	w.Name(path.Join(u.prefix, title))
 	u.win[title] = w
@@ -163,19 +207,26 @@ func (u *UI) new(title string) *win {
 	return w
 }
 
+// show forces a window to the top and returns it, if it's found.
 func (u *UI) show(title string) *win {
 	u.Lock()
 	defer u.Unlock()
 	if w, ok := u.win[title]; ok {
+		debug("showing: %q\n", title)
 		w.Ctl("show")
 		return w
 	}
 	return nil
 }
 
+// look is show-or-create.
+//
+// It understands a few magic strings to facilitate this.
 func (u *UI) look(title string) bool {
+	title = strings.TrimPrefix(title, u.prefix)
+	debug("looking: %q\n", title)
 	switch title {
-	case "my-issues":
+	case "my-issues", "mine", "Mine":
 		if w := u.show("my-issues"); w == nil {
 			w = u.new("my-issues")
 			if w == nil {
@@ -184,14 +235,11 @@ func (u *UI) look(title string) bool {
 			w.Ctl("cleartag")
 			w.Fprintf("tag", " New Get Search ")
 			w.reload = u.fetchMine
-			w.Fprintf("data", "Loading...\n")
 			w.reload(w)
 		}
 		return true
-	case "Projects":
-		//
-	case "Issues", "Search":
-		//
+	case "Projects", "Issues", "Search":
+		u.err(fmt.Sprintf("%q not implemented yet\n", title))
 	}
 	if u.projRe.MatchString(title) {
 		if w := u.show(title); w == nil {
@@ -201,7 +249,8 @@ func (u *UI) look(title string) bool {
 			w.Ctl("cleartag")
 			w.Fprintf("tag", " New Get Search Put Advance ")
 			w.reload = u.fetchIssue(title)
-			w.Fprintf("data", "Loading...\n")
+			w.put = u.putIssue(title)
+			w.Issue = true
 			w.reload(w)
 		}
 		return true
@@ -269,6 +318,39 @@ func (u *UI) fetchIssue(id string) func(*win) {
 		w.Addr("0")
 		w.Ctl("dot=addr")
 		w.Ctl("show")
+	}
+}
+
+func (u *UI) putIssue(id string) func(*win) {
+	return func(w *win) {
+		w.Addr(`#0`)
+		w.Addr(`/^\n/,/^\nReported by/`)
+		q0, q1, err := w.ReadAddr()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		q1 -= len("\nReported by")
+		w.Addr(`#%d,#%d`, q0, q1)
+		b, err := w.ReadAll("xdata")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if len(b) == 0 {
+			return
+		}
+
+		c := jira.Comment{
+			Body: strings.TrimSpace(string(b)) + "\n",
+		}
+
+		debug("putIssue add comment:%q\n", c.Body)
+		if cr, res, err := u.j.Issue.AddComment(id, &c); err != nil {
+			debug("returned comment: %#v\n", cr)
+			debug("returned response: %#v\n", res)
+			log.Printf("error posting comment: %v\n", err)
+		}
 	}
 }
 
